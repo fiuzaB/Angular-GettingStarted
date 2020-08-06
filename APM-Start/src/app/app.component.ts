4b2f590109fffddcdad616cb9803ca1e5d6c9d3d
Fiuza <bfiuza@ciandt.com>
2020-08-06 17:38:23 -0300

    Demo: Creating the App Component - Não finalizado

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  template:
+});
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
-}
+  private pageTitle: string;
+};
